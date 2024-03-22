42f8f522ecfe5217c581317a9829f988261f8d9b
LuciIacoboaea <liacoboaea@gmail.com>
2024-03-22 23:11:09 +0200

    diagrama+setup

---
+++ siteWeb/src/test/java/com/example/siteWeb/SiteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siteWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
